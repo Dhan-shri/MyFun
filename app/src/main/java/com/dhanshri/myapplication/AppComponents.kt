9f07a1838f6908540cb0937eb40bf07403287f25
Dhanshri <dhanshri20@navgurukul.org>
2024-01-05 22:31:18 +0530

    initial commit

---
+++ app/src/main/java/com/dhanshri/myapplication/AppComponents.kt
@@ -0,0 +1,139 @@
+package com.dhanshri.myapplication
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TopBar(value : String,) {
+    Row (modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        ){
+        Text(
+            text = value,
+            color = Color.Black,
+            fontSize = 24.sp,
+           fontWeight = FontWeight.Medium,
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+        Image(
+            modifier = Modifier.size(80.dp),
+            painter = painterResource(id = R.drawable.img_1),
+            contentDescription = "Native Mobile Bits")
+    }
+}
+
+@Preview
+@Composable
+fun TopBarPreview() {
+    TopBar("hi there")
+}
+
+
+@Composable
+fun TextComponent(txtValue:String, textSize : TextUnit, colorValue: Color = Color.Black) {
+
+    Text(
+        text = txtValue,
+        color = colorValue,
+        fontSize = textSize,
+        fontWeight = FontWeight.Black,
+    )
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextComponentPreview() {
+    TextComponent("hi there", 24.sp, Color.Black)
+}
+
+
+@Composable
+fun TextFieldComponent(
+    onTextChanged : (name: String) -> Unit
+) {
+    var currentValue by remember {
+        mutableStateOf("")
+    }
+    val localFocusManager = LocalFocusManager.current
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = currentValue,
+        onValueChange = {
+            currentValue = it
+            onTextChanged(it)
+        },
+        placeholder = {
+            Text(text = "Enter your name", fontSize = 18.sp)
+        },
+        textStyle = TextStyle.Default.copy(fontSize = 24.sp),
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+        keyboardActions = KeyboardActions{
+            // for any custome action after click on keyboard
+            localFocusManager.clearFocus()
+        }
+    )
+}
+@Preview
+@Composable
+fun TextFieldComponentPreview() {
+    TextFieldComponent(onTextChanged = {  })
+}
+
+
+@Composable
+fun AnimalCard(image : Int){
+    Card(
+        modifier = Modifier
+            .padding(24.dp)
+            .size(130.dp),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Image(
+            modifier = Modifier
+                .padding(16.dp)
+                .wrapContentHeight()
+                .wrapContentWidth(),
+            painter = painterResource(id = image),
+            contentDescription = "Animal Image"
+        )
+    }
+}
+
+@Preview
+@Composable
+fun AnimalCardPreview() {
+    AnimalCard(0)
+}
---
+++ app/src/main/java/com/dhanshri/myapplication/screens/UserInputScreen.kt
@@ -0,0 +1,80 @@
+package com.dhanshri.myapplication.screens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.dhanshri.myapplication.AnimalCard
+import com.dhanshri.myapplication.R
+import com.dhanshri.myapplication.TextComponent
+import com.dhanshri.myapplication.TextFieldComponent
+import com.dhanshri.myapplication.TopBar
+import com.dhanshri.myapplication.ui.theme.UserDataUiEvents
+import com.dhanshri.myapplication.ui.theme.UserInputViewModel
+
+
+@Composable
+fun UserInputScreen(userInputViewModel: UserInputViewModel) {
+    Surface(modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(18.dp)
+        )
+        {
+//            TopBar("Hi there \uD83D\uDC4B")
+            TopBar("Hi there \uD83D\uDE0A")
+
+            TextComponent(
+                txtValue = "Let's learn about you!",
+                textSize = 24.sp)
+
+            Spacer(modifier = Modifier.size(20.dp))
+
+            TextComponent(
+                txtValue = "This app will prepare a details page based on input provided by you !",
+                textSize = 18.sp)
+
+            Spacer(modifier = Modifier.size(26.dp))
+
+            TextComponent(txtValue = "Name", textSize = 18.sp)
+            Spacer(modifier = Modifier.size(10.dp))
+            TextFieldComponent(onTextChanged = {
+                userInputViewModel.onEvent(
+                    UserDataUiEvents.UserNameEntered(it)
+                )
+            })
+
+            Spacer(modifier = Modifier.size(20.dp))
+
+            TextComponent(txtValue = "What do you like", textSize = 18.sp)
+
+
+            Row(modifier = Modifier.fillMaxWidth()){
+                AnimalCard(image = R.drawable.cat)
+                AnimalCard(image = R.drawable.dog)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun UserInputScreenPreview() {
+    UserInputScreen(UserInputViewModel())
+}
